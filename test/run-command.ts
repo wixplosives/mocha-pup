406d5026c93bbe4907cd19288d5c04a43629b14e
Avi Vahl <avi.vahl@wix.com>
2018-12-05 23:25:22 +0200

    Update tests to use async spawning also uses shell when launching hoping this will fix Windows travis

--- test/run-command.ts
+++
@@ -1,7 +0,0 @@
-import { spawnSync, SpawnSyncOptions } from 'child_process'
-
-export function runCommand(command: string, options?: SpawnSyncOptions): { output: string, exitCode: number } {
-    const [execName, ...args] = command.split(' ')
-    const { output, status: exitCode } = spawnSync(execName, args, options)
-    return { output: output.join('\n'), exitCode }
-}
